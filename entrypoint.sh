e28a949e4ae2eeb1db7e7c91006231fc4cff8c17
face0u0 <me@face0u0.net>
2021-10-19 13:41:38 +0900

    fixed username

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 指定された環境変数でuser&group切り替え
-sudo usermod -d ${HOME} -u ${USER_ID:-1000} -o -m ${USER}
-sudo groupmod -g ${GROUP_ID:-1000} ${USER}
-sudo chown ${USER} /tex
+sudo usermod -d ${HOME} -u ${USER_ID:-1000} -o -m $(whoami)
+sudo groupmod -g ${GROUP_ID:-1000} $(whoami)
+sudo chown $(whoami) /tex
 
 if [ "$1" != "" ]; then
     exec "$@"
